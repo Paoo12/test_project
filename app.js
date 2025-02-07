2eb9d50de861a461c1c5a707b6215d293761c702
Paoo12 <paolosalvador28@gmail.com>
2025-02-06 18:10:06 -0700

    update 3

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const PORT = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.get('/', (req, res) =>{
+    res.render('index');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, ()=>{
+    console.log(`Connected on ${PORT}`);
 });
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,18 +1,52 @@
-describe('Login Page', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+//test suite
+describe('Login Page', () =>{
+  beforeEach(() =>{
+    cy.visit('http://localhost:3000');
   });
 
-  it('should login with valid credentials', () => {
+
+  //Positive testing
+  it('should login with valid credentials', () =>{
     //Arrange
     cy.get('#email').type('test@example.com');
     cy.get('#password').type('password123');
+
     //Act
     cy.get('#login-button').click();
-    
+
     //Assert
-    cy.on('window:alert', (str) => {
-      expect(str).to.contains('Login Successful');
+    cy.on('window:alert', (txt) =>{
+      expect(txt).to.contains('Login Successful!')
     });
+  });
+
+  //Negative testing
+  it('should display error message if wrong credentials', () =>{
+    //Arrange
+    cy.get('#email').type('test@examples.com');
+    cy.get('#password').type('password123');
+
+    //Act
+    cy.get('#login-button').click();
+
+    //Assert
+    cy.get('#error-message').should(
+      'contain',
+      'Invalid email or password'
+  );
+});
+it('Should only allow 1 submit', () =>{
+  //Arrange
+  // cy.get('#email').type('test@examples.com');
+  //   cy.get('#password').type('password123');
+
+    //Act
+    for(var i = 0; i < 3; i++){
+      cy.get('#login-button').click();
+    }
+
+  // cy.get('#submitCount').should('not.have.text', '1');
+  cy.get('window:alert', (txt) =>{
+    expect(txt).to.equal('You have exceeded the maximum number of attempts')});
   });
 });

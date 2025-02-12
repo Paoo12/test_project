ae1af181fae96dd61683a599bd3b2497b9f58df6
Paoo12 <paolosalvador28@gmail.com>
2025-02-11 18:45:31 -0700

    double 8

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,52 +1,41 @@
 //test suite
 describe('Login Page', () =>{
-  beforeEach(() =>{
-    cy.visit('http://localhost:3000');
-  });
+  //pre-conditions. alternatively, we can use afterEach for post-conditions
+    beforeEach(() =>{
 
-
-  //Positive testing
-  it('should login with valid credentials', () =>{
-    //Arrange
-    cy.get('#email').type('test@example.com');
-    cy.get('#password').type('password123');
-
-    //Act
-    cy.get('#login-button').click();
-
-    //Assert
-    cy.on('window:alert', (txt) =>{
-      expect(txt).to.contains('Login Successful!')
+      cy.visit('http://localhost:3000');
     });
-  });
 
-  //Negative testing
-  it('should display error message if wrong credentials', () =>{
-    //Arrange
-    cy.get('#email').type('test@examples.com');
-    cy.get('#password').type('password123');
 
-    //Act
-    cy.get('#login-button').click();
+    //Positive testing
+    it('should login with valid credentials', () =>{
+      //Arrange
+      cy.get('#email').type('test@example.com');
+      cy.get('#password').type('password123');
 
-    //Assert
-    cy.get('#error-message').should(
-      'contain',
-      'Invalid email or password'
-  );
-});
-it('Should only allow 1 submit', () =>{
-  //Arrange
-  // cy.get('#email').type('test@examples.com');
-  //   cy.get('#password').type('password123');
-
-    //Act
-    for(var i = 0; i < 3; i++){
+      //Act
       cy.get('#login-button').click();
-    }
 
-  // cy.get('#submitCount').should('not.have.text', '1');111112
-  cy.get('window:alert', (txt) =>{
-    expect(txt).to.equal('You have exceeded the maximum number of attempts')});
+      //Assert
+      cy.on('window:alert', (txt) =>{
+        expect(txt).to.contains('Login Successful!')
+      });
+    });
+
+    //Negative testing
+    it('should display error message if wrong credentials', () =>{
+      //Arrange
+      cy.get('#email').type('test@examples.com');
+      cy.get('#password').type('password123');
+
+      //Act
+      cy.get('#login-button').click();
+
+      //Assert
+      cy.get('#error-message').should(
+        'contain',
+        'Invalid email or password'
+    );
   });
+
 });
